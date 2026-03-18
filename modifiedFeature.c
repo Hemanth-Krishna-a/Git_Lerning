8d3a9683b29dbae2b7b4f5f96a6c1146e79f8902
Hemanth Krishna <hemanthkrishna.ab@anorasolutions.com>
2026-03-18 14:34:48 +0530

    modifed fucntion in featurebranch2

---
+++ modifiedFeature.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+struct Node{
+    int data ;
+    struct Node * next;
+};
+
+
+
+void modifiedFeature(int a){
+    printf("this is a modified file version in feature2 bracnh");
+}
